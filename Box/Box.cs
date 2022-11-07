12af20dff5d5f29674edf94de0b8a4b97f7b1964
Marcos Andrade <mvpdeandrade@gmail.com>
2022-11-07 13:46:26 -0300

    Encapsulate Length (private)

--- Box/Box.cs
+++ Box/Box.cs
@@ -8,14 +8,23 @@ namespace Box
 {
     internal class Box
     {
-        private int length;
+        private int length = 3;
         public int height;
         public int width;
         public int volume;
 
+        public int GetLength()
+        {
+            return this.length;
+        }
+        private void SetLength(int length)
+        {
+            //this.length refere-se ao length da classe, e não ao parâmetro da função
+            this.length = length;
+        }
         public void DisplayInfo()
         {
-            Console.WriteLine("The box volume is: {0}", length * height * width);
+            Console.WriteLine("The box volume is: {0}",volume = length * height * width);
         }
     }
 }
--- Box/Program.cs
+++ Box/Program.cs
@@ -7,9 +7,10 @@ namespace Box
         static void Main(string[] args)
         {
             Box box = new Box();
-            //box.length = 3;
+            //box.SetLength(4);
             box.height = 4;
             box.width = 5;
+            Console.WriteLine("Box length is " + box.GetLength());
             box.DisplayInfo();
         }
     }
