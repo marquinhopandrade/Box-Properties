3bf35a129db7de1688c6ddd5abbdce027bf2bbf4
Marcos Andrade <mvpdeandrade@gmail.com>
2022-11-07 13:33:18 -0300

    Declara dimensões

---
+++ Box/Box.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Box
+{
+    internal class Box
+    {
+        private int length;
+        public int height;
+        public int width;
+        public int volume;
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine("The box volume is: {0}", length * height * width);
+        }
+    }
+}
--- Box/Program.cs
+++ Box/Program.cs
@@ -6,7 +6,11 @@ namespace Box
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Box box = new Box();
+            //box.length = 3;
+            box.height = 4;
+            box.width = 5;
+            box.DisplayInfo();
         }
     }
 }
